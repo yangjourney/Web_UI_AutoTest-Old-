b47f9637b100c2cba0ed29d6facd32571cd19bc5
Administrator <yangbo145@hotmail.com>
2017-04-05 16:52:02 +0800

    Web UI Auto Test

---
+++ Test_Case/ysh_LS/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on 2017年3月16日
+
+@author: Administrator
+'''
---
+++ ceshi.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import unittest,time
+from Common.Selenium_Webdriver import webutils
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.driver = webutils()
+        self.driver.wait(20)
+
+    def testName(self):
+        driver = self.driver
+        driver.get("http://www.baidu.com/")
+        driver.send_keys("xpath,.//*[@id='kw']", "ceshi")
+        driver.click("id,su")
+        time.sleep(5)
+        self.assertEqual(driver.get_title(), u"测试_百度搜索") 
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
